c99c502edebe27f55049b3a1f09508e3f2315ef0
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-03-28 08:56:50 +0530

    shared preferance

---
+++ app/src/main/java/com/vimukthini/android/preferenceshared/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.vimukthini.android.preferenceshared
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sharedPreferences = getSharedPreferences("SP_INFO", MODE_PRIVATE)
+
+        saveBtn.setOnClickListener{
+
+            val str = stringEt.text.toString().trim()
+            val swt = switch1.isChecked
+
+            val editor=sharedPreferences.edit()
+
+            editor.putString("String",str)
+            editor.putBoolean("Boolean_value",swt)
+            editor.apply()
+        }
+
+        showDataBtn.setOnClickListener{
+
+            val str = sharedPreferences.getString("String","")
+            val switch1 = sharedPreferences.getBoolean("Boolean_value",false)
+
+            InfoTv.text="String : $str \nBoolean value : $switch1"
+
+        }
+    }
+}
